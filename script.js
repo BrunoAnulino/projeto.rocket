30be88048f1feaaaa3a8725943f3bdb045134bee
BrunoAnulino <113637069+BrunoAnulino@users.noreply.github.com>
2025-03-25 20:10:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,12 +9,12 @@ const img = document.querySelector("#profile img")
  if(html.classList.contains("light")) {
 // se tiver light mode, adicinar a imagem light
 img.setAttribute("src", "./avatar-light.png")
-img.setAttribute( "alt", "Foto de Bruno Anulino")
+
  } else{
 
 
 // set tiver sem light mode, manter a imagem normal
 img.setAttribute("src", "./avatar.png")
-img.setAttribute( "alt", "Foto de Bruno Anulino, com oculos escuro ")
- }
+
+ }else{
 }
